6e667a74c68ed47eb6fcdcd461604de7709bc7e9
Abhishek Upadhyay <https://github.com/Abhishek4209>
2025-07-30 09:24:42 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+from typing import List
+
+
+
+setup(
+    name='medicalChatboat',
+    version='0.0.0',
+    author='Abhishek Upadhyay',
+    author_email='abhishekupadhyay9336@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
